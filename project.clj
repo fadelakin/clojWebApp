595e3dad802db5b2ade907554cbc43de34f1d463
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-12-29 15:17:25 -0500

    app is suitable for deployment

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject cloj-webapp "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :main cloj-webapp.handler
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.2"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [com.h2database/h2 "1.3.170"]]
+                 [com.h2database/h2 "1.3.170"]
+                 [ring/ring-jetty-adapter "1.2.1"]] ; see clojars for current version
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler cloj-webapp.handler/app}
   :profiles
--- src/cloj_webapp/handler.clj
+++ src/cloj_webapp/handler.clj
@@ -2,7 +2,9 @@
 	(:require [cloj-webapp.views :as views]
 		[compojure.core :as cc]
 		[compojure.handler :as handler]
-		[compojure.route :as route]))
+		[compojure.route :as route]
+		[ring.adapter.jetty :as jetty]
+		(:gen-class)))
 
 (cc/defroutes app-routes
 	(cc/GET "/"
@@ -25,3 +27,11 @@
 
 (def app
 	(handler/site app-routes))
+
+(defn -main
+	[& [port]]
+	(let [port (Integer. (or port
+							 (System/getenv "PORT")
+							 5000))]
+	(jetty/run-jetty #'app {:port port
+							:join? false})))
